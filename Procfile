10d4c090008fd18c67ca17a05922bb8223dcba76
Nihal <p.b.nihalcse1@gmail.com>
2026-01-19 01:08:17 +0600

    fix: force migrations and make homepage more defensive

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py migrate --no-input && python manage.py collectstatic --no-input && gunicorn event_management.wsgi
+web: python manage.py migrate --no-input && python manage.py makemigrations --no-input && python manage.py migrate --no-input && python manage.py collectstatic --no-input && gunicorn event_management.wsgi
--- event_management/settings.py
+++ event_management/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-fallback-key-for-production-only-if-missing')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 AUTH_USER_MODEL = 'users.CustomUser'
