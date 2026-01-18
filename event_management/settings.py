4f11519b9d81beed431231238d6e433e744dbf0a
Nihal <p.b.nihalcse1@gmail.com>
2026-01-19 01:02:39 +0600

    debug: enable DEBUG mode to diagnose 500 error

--- event_management/settings.py
+++ event_management/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-fallback-key-for-production-only-if-missing')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
 AUTH_USER_MODEL = 'users.CustomUser'
